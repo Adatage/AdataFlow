27b402181619cc012d61f6432b713298129e4dec
AstrixGame <78021940+astrixgame@users.noreply.github.com>
2024-05-02 19:15:08 +0200

    Update AdataSockets.js

--- src/AdataSockets.js
+++ src/AdataSockets.js
@@ -5,8 +5,45 @@ class AdataSocket {
         WEBRTC: 2
     });
 
-    constructor(uri, protocol) {
-        
+    constructor(uri, protocol, format) {
+        this.uri = uri;
+        this.protocol = protocol;
+        this.format = format;
+        this.connectionState = 0;
+        this.connection = null;
+        this.listeners = {};
+    }
+
+    connect() {
+        switch(this.protocol) {
+            case this.protocol.WEBSOCKET:
+                this.connectionState = 1;
+                try {
+                    this.connection = new WebSocket(this.uri);
+                    this.connectionState = 5;
+                } catch(e) {
+                    this.connectionState = 2;
+                }
+            break;
+            case this.protocol.WEBTRANSPORT:
+
+            break;
+            case this.protocol.WEBRTC:
+
+            break;
+        }
+    }
+
+    disconnect() {
+
+    }
+
+    send() {
+
+    }
+
+    on(name, callback) {
+        this.listeners[name] = callback;
     }
 }
 
