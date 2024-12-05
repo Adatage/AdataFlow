c37f54a09741f9ebb268ed79af4224108da153a3
AstrixGame <78021940+astrixgame@users.noreply.github.com>
2024-12-05 10:03:46 +0100

    Added random identifier generation

--- src/modules/Identificators.js
+++ src/modules/Identificators.js
@@ -2,6 +2,14 @@ class Identificators {
     constructor(instance) {
         this.instance = instance;
     }
+
+    _randName(l = 10) {
+        var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234546789';
+        var className = "";
+        for(var i = 0;i!=l;i++)
+            className += chars.charAt(Math.floor(Math.random() * (chars.length - 1)));
+        return '_'+className;
+    }
 }
 
 export default Identificators;
