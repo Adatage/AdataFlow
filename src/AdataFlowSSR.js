c12f3444d75267099dea7e62157143161d77a0af
AstrixGame <mail.astrixgame@gmail.com>
2024-12-18 23:27:55 +0100

    modified:   src/AdataFlowSSR.js

--- src/AdataFlowSSR.js
+++ src/AdataFlowSSR.js
@@ -15,47 +15,56 @@ class AdataFlowSSR {
         this.content = "";
     }
 
+    _assignBracValues(i, j) {
+        let classMatch;
+        while(classMatch = /\{\{(.*?)\}\}/g.exec(i)) {
+            if(classMatch == null)
+                break;
+            i = i.replaceAll(classMatch[0], this._identifyManager.add(j+classMatch[1]));
+        }
+        return i;
+    }
+
     addContent(component) {
         if(typeof component != 'object' || typeof component.content != 'object')
             return;
 
-        
-
-
-        console.log((component));
-
-        /*
-        if(typeof component != 'object' || typeof component.get != 'function')
-            return;
-        var content = component.get();
-        if(typeof content.css == 'object') {
-            this.styleManager.addVars(content.css.globals);
-            Object.keys(content.css.data).forEach((identificator) => {
-                var idTemp;
+        var processComponent = (i) => {
+            i.create();
+            i.content.html = this._assignBracValues(i.content.html, i.constructor.name+'/');
+            Object.keys(i.content.css.data).forEach((identificator) => {
+                var idTemp, idKtemp;
                 if(identificator.includes("{{")) {
                     idTemp = identificator
-                    let classMatch;
-                    while(classMatch = /\{\{(.*?)\}\}/g.exec(idTemp)) {
-                        if(classMatch == null)
-                            break;
-                        idTemp = idTemp.replaceAll(classMatch[0], this._identifyManager.add(classMatch[1]));
-                    }
+                    idTemp = this._assignBracValues(idTemp, i.constructor.name+'/');
                 } else
-                    idTemp = this._identifyManager.add(identificator.replaceAll('_', '-'));
-                this.styleManager.add(idTemp, content.css.data[identificator]);
+                    idTemp = this._identifyManager.add(i.constructor.name+'/'+identificator.replaceAll('_', '-'));
+                Object.keys(i.content.css.data[identificator]).forEach((k) => {
+                    if(k.includes("{{")) {
+                        idKtemp = k;
+                        idKtemp = this._assignBracValues(idKtemp, i.constructor.name+'/');
+                        i.content.css.data[identificator][idKtemp] = i.content.css.data[identificator][k];
+                        delete i.content.css.data[identificator][k];
+                    }
+                });
+                this.styleManager.add(idTemp, i.content.css.data[identificator]);
+                i.content.css.imports.forEach((imp) => {
+                    if(!this.styleManager.imports.includes(imp))
+                        this.styleManager.imports.push(imp);
+                });
+                this.styleManager.globals = { ...this.styleManager.globals, ...i.content.css.globals };
             });
-        }
-        if(typeof content.html == 'string')
-            this.content += content.html;
-        let classMatch;
-        while(classMatch = /\{\{(.*?)\}\}/g.exec(this.content)) {
-            if(classMatch == null)
-                break;
-            this.content = this.content.replaceAll(classMatch[0], this._identifyManager.add(classMatch[1]));
-        }
-        console.log(this.content);
-        this.content.replace("[[style]]", this.styleManager.generateStyles());
-        */
+            Object.keys(i._childs).forEach((j) => {
+                i._childs[j].forEach((k) => {
+                    if(i.content.html.includes("[["+j+"]]")) {
+                        let child = processComponent(k);
+                        i.content.html = i.content.html.replaceAll("[["+j+"]]", child.html);
+                    }
+                });
+            });
+            return i.content;
+        };
+        this.content = processComponent(component).html.replace("[[style]]", this.styleManager.generateStyles());
     }
 
     render() {
