32e81b1aac204696317edb60009f8fd19981062b
AstrixGame <mail.astrixgame@gmail.com>
2025-01-01 15:37:07 +0100

    modified:   src/AdataFlowSSR.js

--- src/AdataFlowSSR.js
+++ src/AdataFlowSSR.js
@@ -8,11 +8,13 @@ class AdataFlowSSR {
         this._identifyManager = new Identificators(this);
         this.styleManager = new Styles(this, this._options.styles || {});
 
-        this.statusCode = 200;
-        this.headers = {
-            "Content-Type": "text/html;charset=utf-8"
+        this.response = {
+            statusCode: 200,
+            headers: {
+                "Content-Type": "text/html;charset=utf-8"
+            },
+            content: ""
         };
-        this.content = "";
     }
 
     _assignBracValues(i, j) {
@@ -25,52 +27,54 @@ class AdataFlowSSR {
         return i;
     }
 
+    _processComponent(i) {
+        i.create();
+        i.content.html = this._assignBracValues(i.content.html, i.constructor.name+'/');
+        Object.keys(i.content.css.data).forEach((identificator) => {
+            var idTemp, idKtemp;
+            if(identificator.includes("{{")) {
+                idTemp = identificator
+                idTemp = this._assignBracValues(idTemp, i.constructor.name+'/');
+            } else
+                idTemp = this._identifyManager.add(i.constructor.name+'/'+identificator.replaceAll('_', '-'));
+            Object.keys(i.content.css.data[identificator]).forEach((k) => {
+                if(k.includes("{{")) {
+                    idKtemp = k;
+                    idKtemp = this._assignBracValues(idKtemp, i.constructor.name+'/');
+                    i.content.css.data[identificator][idKtemp] = i.content.css.data[identificator][k];
+                    delete i.content.css.data[identificator][k];
+                }
+            });
+            this.styleManager.add(idTemp, i.content.css.data[identificator]);
+            i.content.css.imports.forEach((imp) => {
+                if(!this.styleManager.imports.includes(imp))
+                    this.styleManager.imports.push(imp);
+            });
+            this.styleManager.vars = { ...this.styleManager.vars, ...i.content.css.globals };
+        });
+        Object.keys(i._childs).forEach((j) => {
+            i._childs[j].forEach((k) => {
+                if(i.content.html.includes("[["+j+"]]")) {
+                    let child = this._processComponent(k);
+                    i.content.html = i.content.html.replaceAll("[["+j+"]]", child.html);
+                }
+            });
+        });
+        return i.content;
+    }
+
     addContent(component) {
         if(typeof component != 'object' || typeof component.content != 'object')
             return;
-        var processComponent = (i) => {
-            i.create();
-            i.content.html = this._assignBracValues(i.content.html, i.constructor.name+'/');
-            Object.keys(i.content.css.data).forEach((identificator) => {
-                var idTemp, idKtemp;
-                if(identificator.includes("{{")) {
-                    idTemp = identificator
-                    idTemp = this._assignBracValues(idTemp, i.constructor.name+'/');
-                } else
-                    idTemp = this._identifyManager.add(i.constructor.name+'/'+identificator.replaceAll('_', '-'));
-                Object.keys(i.content.css.data[identificator]).forEach((k) => {
-                    if(k.includes("{{")) {
-                        idKtemp = k;
-                        idKtemp = this._assignBracValues(idKtemp, i.constructor.name+'/');
-                        i.content.css.data[identificator][idKtemp] = i.content.css.data[identificator][k];
-                        delete i.content.css.data[identificator][k];
-                    }
-                });
-                this.styleManager.add(idTemp, i.content.css.data[identificator]);
-                i.content.css.imports.forEach((imp) => {
-                    if(!this.styleManager.imports.includes(imp))
-                        this.styleManager.imports.push(imp);
-                });
-                this.styleManager.vars = { ...this.styleManager.vars, ...i.content.css.globals };
-            });
-            Object.keys(i._childs).forEach((j) => {
-                i._childs[j].forEach((k) => {
-                    if(i.content.html.includes("[["+j+"]]")) {
-                        let child = processComponent(k);
-                        i.content.html = i.content.html.replaceAll("[["+j+"]]", child.html);
-                    }
-                });
-            });
-            return i.content;
-        };
-        this.content = processComponent(component).html.replace("[[@style]]", this.styleManager.generateStyles()).replaceAll(/\[\[(.*?)\]\]/g, '');
+
+        this.response.content = this._processComponent(component).html.replace("[[@style]]", this.styleManager.generateStyles()).replaceAll(/\[\[(.*?)\]\]/g, '');
     }
 
     render() {
         return {
-            statusCode: this.statusCode,
+            statusCode: this.response.statusCode,
             headers: this.headers,
-            body: this.content.replaceAll("\n", "").replaceAll("  ", "")
+            content: this.response.content.replaceAll("\n", "").replaceAll("  ", "")
         };
     }
 }
