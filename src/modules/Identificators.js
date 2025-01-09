8a0d92b568d602bc5e43e41fdd75c435e3c388a7
AstrixGame <mail.astrixgame@gmail.com>
2025-01-09 20:19:15 +0100

    modified:   src/modules/Identificators.js

--- src/modules/Identificators.js
+++ src/modules/Identificators.js
@@ -28,10 +28,10 @@ class Identificators {
 
     _rand(fc = "_", fl = 'x', l = 10) {
         var chars = 'abcdefghijklmnopqrstuvwxyz01234546789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        var className = fc + fl + chars.charAt(Math.floor(Math.random() * (chars.length - 37)));
+        var rnd = fc + fl + chars.charAt(Math.floor(Math.random() * (chars.length - 37)));
         for(var i = 0;i!=l-1;i++)
-            className += chars.charAt(Math.floor(Math.random() * (chars.length - 1)));
-        return className;
+            rnd += chars.charAt(Math.floor(Math.random() * (chars.length - 1)));
+        return rnd;
     }
 }
 
