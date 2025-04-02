59e3b4ed3276a175558623db13f302d78aa957f0
astrixgame <mail.astrixgame@gmail.com>
2025-04-02 22:43:07 +0200

    modified:   www/App.js

--- www/App.js
+++ www/App.js
@@ -1,6 +1,12 @@
 const msg = "Hello World";
 
 export function App() {
-
-    return msg;
+    return (`
+        <Router>
+            <Route host="www.adatage.com" path="/" content="<Layout />">
+                <Route path="" content="<Home />" />
+                <Route path="/news">
+            </Route>
+        </Router>
+    `);
 }
